cc1f16c05d4ead306d7aedb159e98d2998c66323
U-Uranami\Miura Hareaki <miurahareaki@gmail.com>
2015-03-12 15:27:44 +0100

    added authordata

---
+++ Code/AuthorshipAttribution/src/styleRepresentation/AuthorData.java
@@ -0,0 +1,12 @@
+package styleRepresentation;
+
+import java.util.HashMap;
+
+public class AuthorData {
+	private String name;
+	private HashMap<String, Integer> punctuationStats;
+	
+	public float lineLength() {
+		return 42;
+	}
+}
--- Code/AuthorshipAttribution/src/styleRepresentation/DataRepresentation.java
+++ Code/AuthorshipAttribution/src/styleRepresentation/DataRepresentation.java
@@ -1,17 +1,50 @@
 package styleRepresentation;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.List;
 
 public class DataRepresentation {
-
-	public void parse(List<File> array) {
+	
+	
+	
+	public void parseAll(List<File> array) {
 		for (File f : array){
-			//f.open();
-			//Do stuff
-			//f.close();
+			if (f.exists() && f.canRead()) {
+				parse(f);
+			}
+			else {
+				System.out.println("Error with file " + f.getName());
+			}
 		}
 	}
+	
+	public void parse(File f) {
+		BufferedReader br = null;
+		 
+		try {
+			String sCurrentLine;
+			br = new BufferedReader(new FileReader(f));
+			while ((sCurrentLine = br.readLine()) != null) {
+				System.out.println(sCurrentLine);
+			}
+ 
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (br != null)br.close();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+	}
+	public void findPuntuation(String line) {
+		
+	}
+	
 	public static void main(String [] args) {
 		//call julien work
 		DataRepresentation d = new DataRepresentation();
