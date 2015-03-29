6d10fd87cb021cdec10058728fc5db9eecdaf617
GPrev <gabriel.prevosto@insa-rennes.fr>
2015-03-29 13:35:59 +0200

    Added method to bluid stats from confusion matrix

--- Code/AuthorshipAttribution/src/Stats/MulticlassStat.java
+++ Code/AuthorshipAttribution/src/Stats/MulticlassStat.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class MulticlassStat implements Statistician {
 	List<BinaryStat> _classStats;
 	int _totalCount;
-	
+
 	public MulticlassStat(List<Integer> classCounts, List<Integer> classPrediction, List<Integer> classWellPredicted, int totalCount)
 	{
 		_totalCount = totalCount;
@@ -16,6 +16,73 @@ public class MulticlassStat implements Statistician {
 		{
 			_classStats.add(new BinaryStat(totalCount, classCounts.get(i), classPrediction.get(i), classWellPredicted.get(i)));
 		}
+	}
+	
+	public static MulticlassStat makeMulticlassStat(List<List<Integer>> confusionMatrix)
+	{
+		if(confusionMatrix == null || confusionMatrix.size() == 0 || confusionMatrix.get(0).size() == 0)
+			return null;
+		//else
+		return new MulticlassStat(classCounts(confusionMatrix), classPrediction(confusionMatrix), classWellPredicted(confusionMatrix), totalCount(confusionMatrix));
+	}
+	
+	private static int totalCount(List<List<Integer>> confusionMatrix)
+	{
+		//sum of all elements
+		int sum = 0;
+		for(List<Integer> l : confusionMatrix)
+		{
+			for(Integer i : l)
+			{
+				sum += i;
+			}
+		}
+		return sum;
+	}
+
+	private static List<Integer> classCounts(List<List<Integer>> confusionMatrix)
+	{
+		List<Integer> counts = new ArrayList<Integer>();
+		for(List<Integer> l : confusionMatrix)
+		{
+			//sum of every prediction made on class l
+			int count = 0;
+			for(Integer i : l)
+			{
+				count += i;
+			}
+			counts.add(count);
+		}
+		return counts;
+	}
+	
+	private static List<Integer> classPrediction(List<List<Integer>> confusionMatrix)
+	{
+		List<Integer> predictions = new ArrayList<Integer>();
+		int sizex = confusionMatrix.size();
+		int sizey = confusionMatrix.get(0).size();
+		for(int j = 0; j < sizey; ++j)
+		{
+			//sum of every item predicted of class j
+			int count = 0;
+			for(int i = 0; i < sizex; ++i)
+			{
+				count += confusionMatrix.get(i).get(j);
+			}
+			predictions.add(count);
+		}
+		return predictions;
+	}
+	
+	private static List<Integer> classWellPredicted(List<List<Integer>> confusionMatrix)
+	{
+		List<Integer> goodPredictions = new ArrayList<Integer>();
+		for(int i = 0; i < confusionMatrix.size(); ++i)
+		{
+			//adds number of class i predicted i
+			goodPredictions.add(confusionMatrix.get(i).get(i));
+		}
+		return goodPredictions;
 	}
 
 	@Override
