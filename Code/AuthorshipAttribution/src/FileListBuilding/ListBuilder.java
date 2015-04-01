7341518e5fe7186ebb85366ba208d0208fb00200
julien bouvet <julien.bouvet@insa-rennes.fr>
2015-04-01 22:52:14 +0200

    confusion matrix classifier 1

--- Code/AuthorshipAttribution/src/FileListBuilding/ListBuilder.java
+++ Code/AuthorshipAttribution/src/FileListBuilding/ListBuilder.java
@@ -1,6 +1,7 @@
 package fileListBuilding;
 import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * 
@@ -11,8 +12,8 @@ import java.util.ArrayList;
  *
  */
 public class ListBuilder {
-	
-	
+	private static ArrayList<Article> articles = null;
+	private static HashMap<String, Integer> authors = null;
 
 	/**
 	 * @param args
@@ -24,27 +25,54 @@ public class ListBuilder {
 		}
 		
 		System.out.println(buildList(args[0]));
+		System.out.println(authorList().toString());
 		
 	}
 	
 	//"../Data/Reuters50_50/C50test/AaronPressman"
 	//cet argument génère une nullpointerexception ... Try catch ?
 	public static ArrayList<Article> buildList(String path){
+		if (articles == null){
 			ArrayList<Article> returnList = new ArrayList<Article>();
-			
+			if (authors == null)
+				authors = new HashMap<String, Integer>();
 			File mainFile = new File(path);
 			String author;
+			int i = 0;
 			for (File f : mainFile.listFiles()) {
 				
 				author = f.getName();
-											
+				authors.put(author, i);
+				
 				for (File ff : f.listFiles()) {
 					returnList.add(new Article(ff, author));
 				}
-								
+				++i;				
 			}
-			return returnList;
+			articles = returnList;
+		}
+		else{
+			ArrayList<Article> returnList = new ArrayList<Article>();
+			File mainFile = new File(path);
+			String author;
+			int i = 0;
+			for (File f : mainFile.listFiles()) {
+				
+				author = f.getName();
+								
+				for (File ff : f.listFiles()) {
+					returnList.add(new Article(ff, author));
+				}
+				++i;				
+			}
+			articles = returnList;
+		}
+			return articles;
 		
+	}
+	
+	public static HashMap<String, Integer> authorList(){
+		return authors;
 	}
 
 }
--- Code/AuthorshipAttribution/src/styleRepresentation/DataRepresentation.java
+++ Code/AuthorshipAttribution/src/styleRepresentation/DataRepresentation.java
@@ -13,14 +13,23 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map.Entry;
 
+import Stats.MulticlassStat;
 import styleClassifier.KNearestNeighborsClassifier;
 import fileListBuilding.*;
 
 public class DataRepresentation {
+	public static List<List<Integer>> confusionM = null;
 	
+	public static List<List<Integer>> getMatrix(){
+		if (confusionM != null)
+			return confusionM;
+		else
+			throw new NullPointerException("Matrice de confusion non initialisée (vous dever parser d'abord)");
+	}
 	
 	
 	public static List<TextData> parseAll(List<Article> artArray) {
+		initMatrix();
 		ArrayList<TextData> textsData = new ArrayList<TextData>();
 		HashMap<String, AuthorData> authData = new HashMap<String, AuthorData>();
 		for (Article article : artArray){
@@ -38,6 +47,8 @@ public class DataRepresentation {
 			System.out.println("Error with file " + article.getCurrent().getName());
 			return null;
 		}
+		
+		
 		byte[] bytes = null;
 		int paragraphs = 1;
 		HashMap<Character, Integer> punctuation = new HashMap<Character, Integer>();
@@ -89,59 +100,112 @@ public class DataRepresentation {
 			
 		}
 		return new TextData(article.getAuthor(), punctuation, numberOfWords, paragraphs,  text.length(), lines.length, mName, abrev);
-		//return lines.length;
-		/*BufferedReader br = null;
-		 
-		try {
-			String sCurrentLine;
-			br = new BufferedReader(new FileReader(f));
-			while ((sCurrentLine = br.readLine()) != null) {
-				System.out.println(sCurrentLine);
-				
-			}
- 
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if (br != null)br.close();
-			} catch (IOException ex) {
-				ex.printStackTrace();
-			}
-		}*/
+
 	}
 	
+	private static void initMatrix() {
+		System.out.println("Initialisation de la matrice de confusion");
+		//if (confusionM == null){
+			int sizeee = ListBuilder.authorList().size();
+			confusionM = new ArrayList<List<Integer>>(sizeee);
+			for(int i=0; i<sizeee; ++i){
+				ArrayList<Integer> tmpl = new ArrayList<Integer>();
+				for (int j = 0; j < sizeee; ++j){
+					tmpl.add(j, 0);
+				}
+				confusionM.add(i, tmpl);
+			}
+		//}
+	}
+
+
 	public static boolean isTitle(String line) {
 		return line.equals(line.toUpperCase());
 	}
 	
 	public static void testAll(List<TextData> trainingSet, List<Article> testSet) {
+		
+		resetMatrix();
 		int k =  10;
 		int goodAnswers = 0;
 		int yolorandom = 0;
 		for (Article art : testSet) {
 			Entry<String, Integer> res = KNearestNeighborsClassifier.getResponse(KNearestNeighborsClassifier.getNeighbors(trainingSet, parse(art), k));
-			if (art.getAuthor().equals(res.getKey())) {
+			
+			String realAuthor = art.getAuthor(); 
+			Integer idRealAuthor = ListBuilder.authorList().get(realAuthor);
+			
+			String computedAuthor = res.getKey(); 
+			Integer idComputedAuthor = ListBuilder.authorList().get(computedAuthor);
+			
+			confusionM.get(idRealAuthor).set(idComputedAuthor, confusionM.get(idRealAuthor).get(idComputedAuthor)+1);
+			
+			if (realAuthor.equals(computedAuthor)) {
 				goodAnswers++;
 				if (res.getValue() == 1){
 					yolorandom++;
 				}
 			}
-			System.out.println("file " + art.getCurrent().getName()+ " " + art.getAuthor() + " "+ res.getKey() + " : " + res.getValue());
+					
+			//System.out.println("file " + art.getCurrent().getName()+ " " + art.getAuthor() + " "+ res.getKey() + " : " + res.getValue());
 		}
 		System.out.println(""+ goodAnswers + "/2500 good answers, " + (((double)goodAnswers)/2500*100) + "%, " + yolorandom + " yolorandom");
 	}
 	
+	private static void resetMatrix() {
+		System.out.println("Reset Matrice");
+		for(int i=0; i<confusionM.size(); ++i){
+			for (int j = 0; j < confusionM.size(); ++j){
+			confusionM.get(i).set(j, 0);
+			}
+		}
+	}
+
+	public static String printMatrix(){
+		String s = "";
+		for(int i=0; i<confusionM.size(); ++i){
+			for (int j = 0; j < confusionM.size(); ++j){
+				s+=confusionM.get(i).get(j);
+			}
+			s+="\n";
+		}
+		return s;
+	}
+
 	public static void main(String [] args) {
 		//record : k=10, 664 on train, 0 en test
 		//1164/2500 good answers, 46.56%, 602 yolorandom train
-		List<Article> listfiles = ListBuilder.buildList("../../Data/Reuters50_50/C50train");
-		//List<Article> listfiles = ListBuilder.buildList("C:/Users/Miura Hareaki/Desktop/Travail/authorship_attribution/Authorship/Data/Reuters50_50/C50test/AaronPressman/");
-		List<TextData> trainingSet = parseAll(listfiles);
+		List<Article> trainSet= ListBuilder.buildList("../../Data/Reuters50_50/C50train/");
+		List<TextData> trainingSet = parseAll(trainSet);
 		TextData.setMinMaxFeatures(trainingSet);
-		listfiles = ListBuilder.buildList("../../Data/Reuters50_50/C50test");
+		
+		
+		//List<Article> listfiles = ListBuilder.buildList("C:/Users/Miura Hareaki/Desktop/Travail/authorship_attribution/Authorship/Data/Reuters50_50/C50test/AaronPressman/");
+		List<Article> testSet = ListBuilder.buildList("../../Data/Reuters50_50/C50test/");
+		
 		//d.parseAll(listfiles);
-		testAll(trainingSet, listfiles);
+		testAll(trainingSet, trainSet);
+		MulticlassStat m = MulticlassStat.makeMulticlassStat(getMatrix());
+		
+		System.out.println("Précision : " + m.accuracy());
+		System.out.println("Recall : " + m.recall());
+		System.out.println("fMesure : " + m.fMeasure());
+		
+		System.out.println(printMatrix());
+		
+		System.out.println("Validation test");
+		
+		testAll(trainingSet, testSet);
+		m = MulticlassStat.makeMulticlassStat(getMatrix());
+		
+		System.out.println("Précision : " + m.accuracy());
+		System.out.println("Recall : " + m.recall());
+		System.out.println("fMesure : " + m.fMeasure());
+		
+		System.out.println(printMatrix());
+		
+		
+		
 		//call classifier
 		//dance macarena
 		//bring beer and stuffs
